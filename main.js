da68db131a298dd4e7a808c8fa8fff90ff7d2f7e
Nicoly <nicolythayna901@gmail.com>
2023-05-10 09:48:58 -0300

    Finaliza formulário de recuperação

---
+++ main.js
@@ -0,0 +1,13 @@
+function Verificar() {
+
+     let name = document.getElementById('loginName').value;
+    
+     let email = document.getElementById('loginEmail').value;
+    
+     let idade = document.getElementById('loginIdade').value;
+    
+     if (!name || !email || !idade)
+    
+     alert("Campos de preenchimento obrigatório. Favor preencher")
+    
+    }
